889300de32d7c635deaf7e6daef4ba02eef7809c
Thevindu Sathsara Gamage <thevidu2007627@gmail.com>
2025-08-30 19:22:10 +0530

    feat: add new actions, change database connet function - added new actions for getPostById, deletePost and updatePost - added new check for database connect function to check whether database is already connected or not

--- src/actions/post.actions.ts
+++ src/actions/post.actions.ts
@@ -5,6 +5,9 @@ import Post from "@/lib/db/models/post.model";
 import { revalidatePath } from "next/cache";
 import { getUserByClerkId } from "./util.action";
 import { IPost } from "@/lib/types/modals.type";
+import Comment from "@/lib/db/models/comment.modal";
+import SavedPost from "@/lib/db/models/savedPost.modal";
+import Notification from "@/lib/db/models/notification.modal";
 
 export type PostFormState = {
   success: boolean;
@@ -41,13 +44,29 @@ export async function createPost(
   }
 }
 
+export async function getPostById(postId: string) {
+  console.log("getting post by postId", postId);
+  try {
+    await connectDB();
+    const post = await Post.findById(postId)
+      .populate("user", "name username clerkId profilePic")
+      .lean();
+
+    return JSON.parse(JSON.stringify(post));
+  } catch (err) {
+    console.error(err);
+    const message = err instanceof Error ? err.message : "An unknown error occurred.";
+    throw new Error(message);
+  }
+}
+
 export async function getAllPosts() {
   try {
     await connectDB();
     const user = await getUserByClerkId();
 
     if (!user) {
-      return { success: false, message: "Unauthorized" };
+      throw new Error("Unauthorized");
     }
 
     const posts = await Post.find({ visibility: "public" })
@@ -56,6 +75,49 @@ export async function getAllPosts() {
       .lean();
 
     return JSON.parse(JSON.stringify(posts));
+  } catch (err) {
+    console.error(err);
+    const message = err instanceof Error ? err.message : "An unknown error occurred.";
+    throw new Error(message);
+  }
+}
+
+export async function deletePost(postId: string) {
+  try {
+    await connectDB();
+    const user = await getUserByClerkId();
+
+    if (!user) {
+      throw new Error("Unauthorized");
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      throw new Error("Post not found");
+    }
+
+    if (post.user.toString() !== user._id) {
+      throw new Error("Unauthorized");
+    }
+
+    await Comment.deleteMany({ post: postId });
+    await SavedPost.deleteMany({ post: postId });
+    await Notification.deleteMany({ post: postId });
+    await Post.findByIdAndDelete(postId);
+    revalidatePath("/");
+  } catch (err) {
+    console.error(err);
+    const message = err instanceof Error ? err.message : "An unknown error occurred.";
+    throw new Error(message);
+  }
+}
+
+export async function updatePost(postId: string, post: IPost) {
+  try {
+    await connectDB();
+    await Post.findByIdAndUpdate(postId, post);
+    revalidatePath("/");
   } catch (err) {
     console.error(err);
     const message = err instanceof Error ? err.message : "An unknown error occurred.";
--- src/lib/db/db.config.ts
+++ src/lib/db/db.config.ts
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+  const connectionState = mongoose.connection.readyState;
+  // Cheking Whether DB is already connected
+  // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
+  if (connectionState === 1) {
+    console.log(`MongoDB already connected: ${mongoose.connection.host}`);
+    return;
+  }
+
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI as string);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
